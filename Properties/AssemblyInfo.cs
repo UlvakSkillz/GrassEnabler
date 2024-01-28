5e18a3c5e65f53e6238662fb2234d35e9afd8302
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2024-01-27 23:39:45 -0800

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using GrassEnabler; // The namespace of your mod class
+// ...
+[assembly: MelonInfo(typeof(main), "Grass Enabler", "1.0.0", "UlvakSkillz")]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GrassEnabler")]
+[assembly: AssemblyDescription("Shows the Grass in the Gym")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GrassEnabler")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f59cac8-7c2e-4613-b6c2-24e7da20d0ce")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ main.cs
@@ -0,0 +1,35 @@
+﻿using MelonLoader;
+using System;
+using UnityEngine;
+
+namespace GrassEnabler
+{
+    public class main : MelonMod
+    {
+        private string currentScene = "";
+        private bool sceneChanged = false;
+
+        public override void OnFixedUpdate()
+        {
+            if ((sceneChanged) && (currentScene == "Gym"))
+            {
+                try
+                {
+                    GameObject.Find("--------------SCENE--------------").transform.GetChild(2).gameObject.SetActive(true);
+                    MelonLogger.Msg("Ping");
+                    sceneChanged = false;
+                }
+                catch (Exception e)
+                {
+                    MelonLogger.Error(e);
+                }
+            }
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            currentScene = sceneName;
+            sceneChanged = true;
+        }
+    }
+}
